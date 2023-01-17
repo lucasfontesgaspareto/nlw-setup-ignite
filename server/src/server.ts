6153d9f74595e92f875b8aa496b4d7eecbdef4ad
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-16 21:27:04 -0300

    feat: setup cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,9 +1,22 @@
+import { PrismaClient } from '@prisma/client'
 import Fastify from 'fastify'
+import cors from '@fastify/cors'
 
 const server = Fastify()
+const prisma = new PrismaClient()
+
+server.register(cors, {
+  origin: ['http://localhost:3000', 'https://habitto.life']
+})
 
 server.get('/', () => {
   return 'Make your habits to reality'
+})
+
+server.get('/habits', async () => {
+  const habits = await prisma.habit.findMany()
+  
+  return habits
 })
 
 server.listen({
