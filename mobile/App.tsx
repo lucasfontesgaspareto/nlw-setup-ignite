223bcdca83d69ea1b143c0c2ddda981d2f9fdc60
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-16 22:33:14 -0300

    [mobile] chore: update style

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -23,7 +23,9 @@ export default function App() {
 
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.text}>
+        Open up App.tsx to start working on your app!
+      </Text>
       <StatusBar style="auto" />
     </View>
   )
@@ -32,8 +34,12 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#09090A',
     alignItems: 'center',
     justifyContent: 'center'
+  },
+  text: {
+    color: '#FFF',
+    fontFamily: 'Inter_800ExtraBold'
   }
 })
