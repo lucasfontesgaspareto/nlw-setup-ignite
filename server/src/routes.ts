40035b54395bd85e8a6e1a12cfa55c63c3db6905
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-17 20:21:17 -0300

    feat(server): create new habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,7 +1,32 @@
 import { FastifyInstance } from 'fastify'
 import { prisma } from './lib/prisma'
+import { z } from 'zod'
+import dayjs from 'dayjs'
 
 export async function appRoutes(server: FastifyInstance) {
+  server.post('/habits', async (request) => {
+    const createHabitBody = z.object({
+      title: z.string(),
+      weekDays: z.array(z.number().min(0).max(6))
+    })
+
+    const { title, weekDays } = createHabitBody.parse(request.body)
+
+    const today = dayjs().startOf('day').toDate()
+
+    await prisma.habit.create({
+      data: {
+        title,
+        weekDays: {
+          create: weekDays.map((day) => ({
+            week_day: day
+          }))
+        },
+        created_at: today
+      }
+    })
+  })
+
   server.get('/habits', async () => {
     const habits = await prisma.habit.findMany()
     return habits
