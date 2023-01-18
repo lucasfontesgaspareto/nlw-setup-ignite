ba3e9e705408be81588eafc4046d51b27712b73d
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 20:17:28 -0300

    feat(mobile): add week days label

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,12 +1,27 @@
 import React from 'react'
-import { View } from 'react-native'
+import { View, Text } from 'react-native'
 import HabitDay from '../../components/HabitDay'
 import Header from '../../components/Header'
+import { DAY_SIZE, WEEK_DAYS } from '../../utils/constants'
 
 function HomeScreen() {
   return (
     <View className="flex-1 bg-dark px-8 pt-16">
       <Header />
+
+      <View className="flex-row mt-6 mb-2">
+        {WEEK_DAYS.map((weekDay, index) => {
+          return (
+            <Text
+              key={`${weekDay}-${index}`}
+              className="text-zinc-400 text-xl font-bold text-center mx-1"
+              style={{ width: DAY_SIZE }}>
+              {weekDay}
+            </Text>
+          )
+        })}
+      </View>
+
       <HabitDay />
     </View>
   )
--- mobile/src/utils/constants.ts
+++ mobile/src/utils/constants.ts
@@ -5,4 +5,5 @@ export const SCREEN_HORIZONTAL_PADDING = (32 * 2) / 5
 
 export const DAY_MARGIN_BETWEEN = 8
 export const DAY_SIZE =
-  Dimensions.get('screen').width / WEEK_DAYS.length - SCREEN_HORIZONTAL_PADDING
+  Dimensions.get('screen').width / WEEK_DAYS.length -
+  (SCREEN_HORIZONTAL_PADDING + 5)
