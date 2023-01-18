66c8d065cff0018cadf52ab66ed0ffadeaaca212
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 19:53:55 -0300

    feat(mobile): add header component

---
+++ mobile/src/components/Header/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function Header() {
+  return <View className="w-full flex-row items-center justify-between"></View>
+}
+
+export default Header
--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import { View } from 'react-native'
+import Header from '../../components/Header'
 
 function HomeScreen() {
-  return <View className=""></View>
+  return (
+    <View className="flex-1 bg-dark px-8 pt-16">
+      <Header />
+    </View>
+  )
 }
 
 export default HomeScreen
