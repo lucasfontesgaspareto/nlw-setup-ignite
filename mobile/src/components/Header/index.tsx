716ee8c941704cc3b94f37c15f73ca295a2a5d77
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 19:59:57 -0300

    feat(mobile): add svg support

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,8 +1,14 @@
 import React from 'react'
 import { View } from 'react-native'
 
+import Logo from '../../assets/logo.svg'
+
 function Header() {
-  return <View className="w-full flex-row items-center justify-between"></View>
+  return (
+    <View className="w-full flex-row items-center justify-between">
+      <Logo />
+    </View>
+  )
 }
 
 export default Header
