abfc4d5472ab2a323cb2b488b27e49ea6a639859
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 19:46:55 -0300

    chore(mobile): setup nativewind

---
+++ mobile/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./App.{js,jsx,ts,tsx}', './src/**/*.{js,jsx,ts,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        dark: '#09090a'
+      },
+      fontFamily: {
+        regular: 'Inter_400Regular',
+        semibold: 'Inter_600SemiBold',
+        bold: 'Inter_700Bold',
+        extrabold: 'Inter_800ExtraBold'
+      }
+    }
+  },
+  plugins: []
+}
