689811e6e4270036861cc62526687f6bf3bb6b52
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 20:05:21 -0300

    feat(mobile): add new habit button

--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { View } from 'react-native'
+import { TouchableOpacity, View, Text } from 'react-native'
+import { Feather } from '@expo/vector-icons'
+import colors from 'tailwindcss/colors'
 
 import Logo from '../../assets/logo.svg'
 
@@ -7,6 +9,13 @@ function Header() {
   return (
     <View className="w-full flex-row items-center justify-between">
       <Logo />
+
+      <TouchableOpacity
+        activeOpacity={0.7}
+        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
+        <Feather name="plus" size={20} color={colors.violet[500]}></Feather>
+        <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
+      </TouchableOpacity>
     </View>
   )
 }
