4b27aead6af3aea4f8c00bb5cb902be3937da35b
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 18:51:23 -0300

    feat(server): add endpoint to get day habits

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -27,9 +27,41 @@ export async function appRoutes(server: FastifyInstance) {
     })
   })
 
-  server.get('/habits', async () => {
-    const habits = await prisma.habit.findMany()
-    return habits
+  server.get('/day', async (request) => {
+    const getDayParams = z.object({
+      date: z.coerce.date()
+    })
+
+    const { date } = getDayParams.parse(request.query)
+
+    const parsedDate = dayjs(date).startOf('day')
+    const weekDay = dayjs(parsedDate).get('day')
+
+    const possibleHabits = await prisma.habit.findMany({
+      where: {
+        created_at: {
+          lte: date
+        },
+        weekDays: {
+          some: {
+            week_day: weekDay
+          }
+        }
+      }
+    })
+
+    const day = await prisma.day.findUnique({
+      where: {
+        date: parsedDate.toDate()
+      },
+      include: {
+        dayHabits: true
+      }
+    })
+
+    const completedHabits = day?.dayHabits.map((dayHabit) => dayHabit.habit_id)
+
+    return { possibleHabits, completedHabits }
   })
 
   server.get('*', () => {
