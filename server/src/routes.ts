5bc2ecd873d327a68c715dfab4aa62f56714e015
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-18 21:01:42 -0300

    feat(server): add toggle day habit endpoint

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -64,6 +64,54 @@ export async function appRoutes(server: FastifyInstance) {
     return { possibleHabits, completedHabits }
   })
 
+  server.patch('/habits/:id/toggle', async (request) => {
+    const toggleHabitParams = z.object({
+      id: z.string().uuid()
+    })
+
+    const { id } = toggleHabitParams.parse(request.params)
+
+    const today = dayjs().startOf('day').toDate()
+
+    let day = await prisma.day.findUnique({
+      where: {
+        date: today
+      }
+    })
+
+    if (!day) {
+      day = await prisma.day.create({
+        data: {
+          date: today
+        }
+      })
+    }
+
+    const dayHabit = await prisma.dayHabit.findUnique({
+      where: {
+        day_id_habit_id: {
+          day_id: day.id,
+          habit_id: id
+        }
+      }
+    })
+
+    if (dayHabit) {
+      await prisma.dayHabit.delete({
+        where: {
+          id: dayHabit.id
+        }
+      })
+    } else {
+      await prisma.dayHabit.create({
+        data: {
+          day_id: day.id,
+          habit_id: id
+        }
+      })
+    }
+  })
+
   server.get('*', () => {
     return 'Make your habits to reality'
   })
