0c1e3261379fd2b43705023ae838d507f04311bc
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-20 19:53:57 -0300

    feat(mobile): add empty screens

---
+++ mobile/src/screens/Habit/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function HabitScreen() {
+  return <View className="flex-1 px-8 pt-16 bg-dark"></View>
+}
+
+export default HabitScreen
---
+++ mobile/src/screens/NewHabit/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { View } from 'react-native'
+
+function NewHabitScreen() {
+  return <View className="flex-1 px-8 pt-16 bg-dark"></View>
+}
+
+export default NewHabitScreen
