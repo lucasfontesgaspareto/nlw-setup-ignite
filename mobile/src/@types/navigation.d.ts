38ae8e37e8eab72e342e72469f59b59bc0bd6dad
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-20 20:15:26 -0300

    feat(mobile): defined routes type

---
+++ mobile/src/@types/navigation.d.ts
@@ -0,0 +1,11 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      home: undefined
+      habit: {
+        date: string
+      }
+      'new-habit': undefined
+    }
+  }
+}
--- mobile/src/components/Header/index.tsx
+++ mobile/src/components/Header/index.tsx
@@ -2,19 +2,23 @@ import React from 'react'
 import { TouchableOpacity, View, Text } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import { useNavigation } from '@react-navigation/native'
 
 import Logo from '../../assets/logo.svg'
 
 function Header() {
+  const { navigate } = useNavigation()
+
   return (
-    <View className="w-full flex-row items-center justify-between">
+    <View className="flex-row items-center justify-between w-full">
       <Logo />
 
       <TouchableOpacity
+        onPress={() => navigate('new-habit')}
         activeOpacity={0.7}
-        className="flex-row h-11 px-4 border border-violet-500 rounded-lg items-center">
+        className="flex-row items-center px-4 border rounded-lg h-11 border-violet-500">
         <Feather name="plus" size={20} color={colors.violet[500]}></Feather>
-        <Text className="text-white ml-3 font-semibold text-base">Novo</Text>
+        <Text className="ml-3 text-base font-semibold text-white">Novo</Text>
       </TouchableOpacity>
     </View>
   )
