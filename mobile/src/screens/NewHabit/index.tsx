681ac855ec046d3f3857b6c82400784d25408cb6
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-20 20:23:32 -0300

    feat(mobile): add habit input

--- mobile/src/screens/NewHabit/index.tsx
+++ mobile/src/screens/NewHabit/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { ScrollView, View } from 'react-native'
+import { ScrollView, View, Text, TextInput } from 'react-native'
 import BackButton from '../../components/BackButton'
 
 function NewHabitScreen() {
@@ -7,6 +7,14 @@ function NewHabitScreen() {
     <View className="flex-1 px-8 pt-16 bg-dark">
       <ScrollView showsVerticalScrollIndicator={false}>
         <BackButton />
+        <Text className="mt-6 text-3xl font-extrabold text-white">
+          Criar Hábito
+        </Text>
+        <Text className="mt-6 text-base font-semibold text-white">
+          Qual seu comprometimento?
+        </Text>
+
+        <TextInput className="h-12 pl-4 mt-3 text-white rounded-lg bg-zinc-800 focus:border-2 focus:border-green-600" />
       </ScrollView>
     </View>
   )
