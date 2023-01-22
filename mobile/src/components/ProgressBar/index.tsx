a4983b9a942971b620fbf0f1a9d0c441c9f839dd
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-22 12:15:10 -0300

    feat(mobile): animate progress bar

--- mobile/src/components/ProgressBar/index.tsx
+++ mobile/src/components/ProgressBar/index.tsx
@@ -1,16 +1,33 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { View } from 'react-native'
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming
+} from 'react-native-reanimated'
 
 type ProgressBarProps = {
   progress?: number
 }
 
 function ProgressBar({ progress = 0 }: ProgressBarProps) {
+  const sharedProgress = useSharedValue(progress)
+
+  const style = useAnimatedStyle(() => {
+    return {
+      width: `${sharedProgress.value}%`
+    }
+  })
+
+  useEffect(() => {
+    sharedProgress.value = withTiming(progress)
+  }, [progress])
+
   return (
     <View className="w-full h-3 mt-4 rounded-xl bg-zinc-700">
-      <View
+      <Animated.View
         className="h-3 rounded-xl bg-violet-600"
-        style={{ width: `${progress}%` }}></View>
+        style={style}></Animated.View>
     </View>
   )
 }
