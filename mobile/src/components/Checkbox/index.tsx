9dab93e8ac75df835fe1bee76d7bfc1698993090
Lucas Fontes Gaspareto <lucasfontesgaspareto@gmail.com>
2023-01-22 12:18:01 -0300

    feat(mobile): add animate in checkbox

--- mobile/src/components/Checkbox/index.tsx
+++ mobile/src/components/Checkbox/index.tsx
@@ -7,6 +7,7 @@ import {
 } from 'react-native'
 import { Feather } from '@expo/vector-icons'
 import colors from 'tailwindcss/colors'
+import Animated, { ZoomIn, ZoomOut } from 'react-native-reanimated'
 
 type CheckboxProps = TouchableOpacityProps & {
   checked?: boolean
@@ -20,9 +21,12 @@ function Checkbox({ checked = false, title, ...props }: CheckboxProps) {
       activeOpacity={0.7}
       className="flex-row items-center mb-2">
       {checked ? (
-        <View className="items-center justify-center w-8 h-8 bg-green-500 rounded-lg">
+        <Animated.View
+          entering={ZoomIn}
+          exiting={ZoomOut}
+          className="items-center justify-center w-8 h-8 bg-green-500 rounded-lg">
           <Feather name="check" size={20} color={colors.white}></Feather>
-        </View>
+        </Animated.View>
       ) : (
         <View className="items-center justify-center w-8 h-8 rounded-lg bg-zinc-900" />
       )}
